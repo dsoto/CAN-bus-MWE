1af8584b1c0d491596fd428fca418d71d23283a8
Daniel Soto <sotod@sonoma.edu>
2019-10-10 11:53:46 -0700

    correctly reads into dictionary

--- read-emits.py
+++ read-emits.py
@@ -1,8 +1,14 @@
+# this reads CAN bus messages from a connection VESC/DBMS combo
+# VESC and DBMS are connected by all four wires
+# DBMS and computer are connected by CANL and CANH only through Canable.io adapter
+
+
 from canard.hw import cantact
 from canard import can
 from canard.utils.queue import CanQueue
 
 import time
+import numpy as np
 
 dev = cantact.CantactDev('/dev/tty.usbmodem14101')
 dev.set_bitrate(500000)
@@ -11,10 +17,27 @@ dev.start()
 cq = CanQueue(dev)
 cq.start()
 
+def get_32(values, scale):
+    values.reverse()
+    return np.frombuffer(bytes(values), dtype=np.uint8).view(dtype=np.int32)[0]/scale
+
 while True:
 
-    # only print frames we are interested in
-    print(cq.recv(filter=0x1E0A, timeout=0.5))
-    print(cq.recv(filter=0x1016, timeout=0.5))
-    print()
+    data = {}
+    # TODO: associate values we want with specific frames to read from
+    # TODO: associate values with bytes within those specific frames
+    # TODO: clean way to gang up stuff
+    # TODO: document frame ids and stuff in them in table in thing
+
+    # grab pack voltage and current from battery
+    frame = cq.recv(filter=0x1E0A, timeout=0.5)
+    data['pack_voltage'] = get_32(frame.data[0:4], 1e5)
+    data['pack_current'] = get_32(frame.data[4:8], 1e5)
+
+    # grab RPM
+    # TODO: document and determine frame ID
+    # frame = cq.recv(filter=?, timeout=0.5)
+    # data['ERPM'] = get_32(frame.data[0:4], 1)
+
+    print(data)
     time.sleep(1)
