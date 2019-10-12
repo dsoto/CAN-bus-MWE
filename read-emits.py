dea7dd2527193d920d43292e796200c99ab64ba2
Daniel Soto <sotod@sonoma.edu>
2019-10-12 11:04:22 -0700

    adds more reads

--- read-emits.py
+++ read-emits.py
@@ -18,26 +18,51 @@ cq = CanQueue(dev)
 cq.start()
 
 def get_32(values, scale):
+    # returns 32 bit value after interpreting list of values in CAN packet as little endian?
     values.reverse()
     return np.frombuffer(bytes(values), dtype=np.uint8).view(dtype=np.int32)[0]/scale
+
+def get_16(values, scale):
+    # returns 16 bit value after interpreting list of values in CAN packet as little endian?
+    values.reverse()
+    return np.frombuffer(bytes(values), dtype=np.uint8).view(dtype=np.int16)[0]/scale
 
 while True:
 
     data = {}
-    # TODO: associate values we want with specific frames to read from
-    # TODO: associate values with bytes within those specific frames
-    # TODO: clean way to gang up stuff
-    # TODO: document frame ids and stuff in them in table in thing
 
     # grab pack voltage and current from battery
     frame = cq.recv(filter=0x1E0A, timeout=0.5)
+    print(frame)
+    # CAN_PACKET_BMS_STATUS_MAIN_IV = 0x1E
     data['pack_voltage'] = get_32(frame.data[0:4], 1e5)
     data['pack_current'] = get_32(frame.data[4:8], 1e5)
 
-    # grab RPM
-    # TODO: document and determine frame ID
-    # frame = cq.recv(filter=?, timeout=0.5)
-    # data['ERPM'] = get_32(frame.data[0:4], 1)
+    # grab pack temperature
+    # CAN_PACKET_BMS_STATUS_TEMPERATURES
+    frame = cq.recv(filter=0x210A, timeout=0.5)
+    print(frame)
+    data['battery_temp'] = get_16(frame.data[6:8], 1e2)
+
+    # grab high and low cell voltage
+    # CAN_PACKET_BMS_STATUS_CELLVOLTAGE
+    frame = cq.recv(filter=0x1F0A, timeout=0.5)
+    print(frame)
+    data['cell_voltage_low'] = get_32(frame.data[0:4], 1e5)
+    data['cell_voltage_high'] = get_32(frame.data[4:8], 1e5)
+
+    # grab RPM and current
+    # CAN_PACKET_STATUS
+    frame = cq.recv(filter=0x0916, timeout=0.5)
+    print(frame)
+    data['rpm'] = get_32(frame.data[0:4], 1)
+    data['motor_current'] = get_16(frame.data[4:6], 1e1)
+
+    # grab fet temp
+    # CAN_PACKET_STATUS_4
+    frame = cq.recv(filter=0x1016, timeout=0.5)
+    print(frame)
+    data['fet_temp'] = get_16(frame.data[0:2], 1e1)
 
     print(data)
     time.sleep(1)
