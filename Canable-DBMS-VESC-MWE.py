c29cba59917b300eaa5465c1bba03af7ebac0b5f
Daniel Soto <sotod@sonoma.edu>
2019-10-31 13:01:50 -0700

    renames python file

--- Canable-DBMS-VESC-MWE.py
+++ Canable-DBMS-VESC-MWE.py
@@ -1,7 +1,10 @@
 # this reads CAN bus messages from a connection VESC/DBMS combo
-# VESC and DBMS are connected by all four wires
+# VESC CAN connector and DBMS CAN connector are connected by all four wires
 # DBMS and computer are connected by CANL and CANH only through Canable.io adapter
+# This example uses the canard python CAN library for message filtering
 
+# in this example, the BMS has CAN device ID 0x0A
+# the VESC has CAN device ID 0x16
 
 from canard.hw import cantact
 from canard import can
@@ -18,51 +21,55 @@ cq = CanQueue(dev)
 cq.start()
 
 def get_32(values, scale):
-    # returns 32 bit value after interpreting list of values in CAN packet as little endian?
+    # reverses list of 4 bytes and interprets as 32 bit integer and scales
     values.reverse()
     return np.frombuffer(bytes(values), dtype=np.uint8).view(dtype=np.int32)[0]/scale
 
 def get_16(values, scale):
-    # returns 16 bit value after interpreting list of values in CAN packet as little endian?
+    # reverses list of 2 bytes and interprets as 16 bit integer and scales
     values.reverse()
     return np.frombuffer(bytes(values), dtype=np.uint8).view(dtype=np.int16)[0]/scale
 
 while True:
 
+    # assemble all data in a dictionary
     data = {}
 
     # grab pack voltage and current from battery
+    # message id is CAN_PACKET_BMS_STATUS_MAIN_IV = 0x1E
+    # device id is 0x0A
     frame = cq.recv(filter=0x1E0A, timeout=0.5)
     print(frame)
-    # CAN_PACKET_BMS_STATUS_MAIN_IV = 0x1E
     data['pack_voltage'] = get_32(frame.data[0:4], 1e5)
     data['pack_current'] = get_32(frame.data[4:8], 1e5)
 
     # grab pack temperature
-    # CAN_PACKET_BMS_STATUS_TEMPERATURES
+    # CAN_PACKET_BMS_STATUS_TEMPERATURES = 0x21
     frame = cq.recv(filter=0x210A, timeout=0.5)
     print(frame)
     data['battery_temp'] = get_16(frame.data[6:8], 1e2)
 
     # grab high and low cell voltage
-    # CAN_PACKET_BMS_STATUS_CELLVOLTAGE
+    # CAN_PACKET_BMS_STATUS_CELLVOLTAGE = 0x1F
     frame = cq.recv(filter=0x1F0A, timeout=0.5)
     print(frame)
     data['cell_voltage_low'] = get_32(frame.data[0:4], 1e5)
     data['cell_voltage_high'] = get_32(frame.data[4:8], 1e5)
 
     # grab RPM and current
-    # CAN_PACKET_STATUS
+    # CAN_PACKET_STATUS = 0x09
+    # device id is 0x16
     frame = cq.recv(filter=0x0916, timeout=0.5)
     print(frame)
     data['rpm'] = get_32(frame.data[0:4], 1)
     data['motor_current'] = get_16(frame.data[4:6], 1e1)
 
     # grab fet temp
-    # CAN_PACKET_STATUS_4
+    # CAN_PACKET_STATUS_4 = 0x10
     frame = cq.recv(filter=0x1016, timeout=0.5)
     print(frame)
     data['fet_temp'] = get_16(frame.data[0:2], 1e1)
 
+    # print out the dictionary
     print(data)
     time.sleep(1)
